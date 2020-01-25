42b04d7bf7cf6027722760c4a5ca80a0e2c63e9e
Max Brauer <ma.brauer@live.de>
2020-01-25 18:19:12 +0100

    fix of autoload path

--- php/DataTest/endpoint.php
+++ php/DataTest/endpoint.php
@@ -10,12 +10,44 @@ use GraphQL\Utils\BuildSchema;
 use GraphQL\Utils\AST;
 use Test\Generated\GraphQL\TypeResolver;
 
+//get input
+if (isset($_POST['operations'])) {
+    $rawInput = $_POST['operations'];
+}
+else {
+    $rawInput = file_get_contents('php://input');
+}
+if (preg_match('/^\-{4,}/', $rawInput)) {
+    $parts = preg_split('/[\r\n]/', $rawInput, -1, PREG_SPLIT_NO_EMPTY);
+    for ($i = 0; $i < count($parts); ++$i) {
+        if (preg_match('/name\=\"operations\"$/', $parts[$i])) {
+            $rawInput = $parts[$i + 1];
+            break;
+        }
+    }
+}
+
+//check cache
+$nocache = isset($_GET['nocache']);
+if (!$nocache) {
+    $key = sha1($rawInput);
+    $matches = array();
+    preg_match('/^([a-z0-9]{2})([a-z0-9]{2})([a-z0-9]+)$/i', $key, $matches);
+    $path = __DIR__ . '/../test/cache/' . $matches[1] 
+        . '/' . $matches[2] . '/' . $matches[3];
+    if (is_file($path) && time() - filemtime($path) < 300) {
+        header('Content-Type: application/json');
+        readfile($path);
+        return;
+    }
+}
+
 //get schema
 
 $cacheFilename = __DIR__ . '/../test/cached_schema.php';
 $sourceFileName = __DIR__ . '/../test/GraphQL/db-schema.part.graphql';
 
-if (!file_exists($cacheFilename) || filemtime($cacheFilename) < filemtime($sourceFileName)) {
+if (!file_exists($cacheFilename) || filemtime($cacheFilename) <= filemtime($sourceFileName)) {
     $document = Parser::parse(
         file_get_contents(
             $sourceFileName
@@ -45,21 +77,6 @@ $typeConfigDecorator = function ($config, $typeNode, $typeMap) {
 $schema = BuildSchema::build($document, $typeConfigDecorator);
 
 //handle request
-if (isset($_POST['operations'])) {
-    $rawInput = $_POST['operations'];
-}
-else {
-    $rawInput = file_get_contents('php://input');
-}
-if (preg_match('/^\-{4,}/', $rawInput)) {
-    $parts = preg_split('/[\r\n]/', $rawInput, -1, PREG_SPLIT_NO_EMPTY);
-    for ($i = 0; $i < count($parts); ++$i) {
-        if (preg_match('/name\=\"operations\"$/', $parts[$i])) {
-            $rawInput = $parts[$i + 1];
-            break;
-        }
-    }
-}
 $input = json_decode($rawInput, true);
 $query = $input['query'];
 $variableValues = isset($input['variables'])
@@ -75,6 +92,7 @@ try {
         $variableValues
     );
     $output = $result->toArray(Debug::INCLUDE_DEBUG_MESSAGE | Debug::INCLUDE_TRACE);
+    TypeResolver::flushSaveBuffer();
 }
 catch (\Exception $e) {
     $output = [
@@ -89,4 +107,11 @@ catch (\Exception $e) {
 // $output['raw'] = $rawInput;
 
 header('Content-Type: application/json');
-echo json_encode($output);
+$result =  json_encode($output);
+if (!$nocache) {
+    $dir = dirname($path);
+    if (!is_dir($dir))
+        mkdir($dir, 0777, true);
+    file_put_contents($path, $result);
+}
+echo $result;
--- php/build.php
+++ php/build.php
@@ -1,6 +1,8 @@
 <?php
 
-require_once __DIR__ . '/../vendor/autoload.php';
+if (is_file(__DIR__ . '/../vendor/autoload.php'))   
+    require_once __DIR__ . '/../vendor/autoload.php';
+else require_once __DIR__ . '/../../../autoload.php';
 
 (function ()  {
     $start = microtime(true);
